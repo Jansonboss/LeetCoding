33acd143fe3cbc3260bdaa5d9e498a0fd3f9af41
Jansonboss <yetao5397@gmail.com>
2021-08-06 23:08:30 -0700

    add lt121 best time to sell and buy stock

---
+++ LinkedList_Array_Number/LeetCode121_BestTimeBuySellStock.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+# Two pointer methods
+
+def max_profit_two_pointers(nums):
+
+	if not nums: return None
+
+	left, right, max_profit = 0, 0, float("-inf")
+
+	while left < len(nums) or right < len(nums):
+		if nums[right] < left[left]:
+			left = right
+			right = right + 1
+		else:
+			profit = nums[right] - nums[left]
+			max_profit = max(profit, max_profit)
+			right += 1
+	return max_profit
