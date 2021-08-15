faeee47bb1c84770caa39d27c72d4adef81ad583
Jansonboss <yetao5397@gmail.com>
2021-08-15 13:46:54 -0700

    add lt34 add find last and first element in rhe sorted array

---
+++ BinarySearch/LeetCode34_FindFirstLastELE_SortedArray.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+def searchRange(nums, target):
+
+
+	if not nums: return [-1, -1]
+	start, end = 0, len(nums) - 1
+	left, right = -1, -1
+
+	while start + 1 < end:
+		mid = (start + end) // 2
+		if nums[mid] <= target:
+			start = mid
+		else:
+			end = mid
+
+	if nums[start] == target:
+		right = start
+	if nums[end] == target:
+		right = end
+
+
+	start, end = 0, len(nums) - 1
+	while start + 1 < end:
+		mid = (start + end) // 2
+		if nums[mid] >= target:
+			end = mid
+		else:
+			start = mid
+
+	if nums[end] == target:
+		left = end
+	if nums[start] == target:
+		left = start
+
+	return [left, right]
